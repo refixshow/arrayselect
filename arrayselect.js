a0e9aa38913284035c9cc525f6229200f680166e
refixshow <adasscieszka@wp.pl>
2020-09-02 15:55:32 +0200

    first commit

---
+++ arrayselect.js
@@ -0,0 +1,15 @@
+Array.prototype.select = function (startIdx = 0, endIdx = 0, step = 1) {
+  let temp = this;
+
+  if (startIdx !== 0 && endIdx !== 0) temp = temp.slice(startIdx, endIdx);
+  else if (startIdx !== 0) temp = temp.slice(startIdx);
+  else if (endIdx !== 0) temp = temp.slice(0, endIdx);
+
+  if (step > 1) {
+    temp = temp.filter((_, idx) => idx % step === 0);
+  }
+
+  return temp;
+};
+
+const arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
