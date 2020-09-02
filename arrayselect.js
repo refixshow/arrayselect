d8102cebe94a21e92162fdfa526ae3eb334d3622
refixshow <adasscieszka@wp.pl>
2020-09-02 19:21:19 +0200

    tego

--- arrayselect.js
+++ arrayselect.js
@@ -1,19 +1,17 @@
 function select(startIdx = 0, endIdx = 0, step = 1) {
-  let temp = this;
+  let temp = this
 
-  if (Math.abs(startIdx) < Math.abs(endIdx))
-    temp = temp.slice(endIdx, startIdx);
-  else if (startIdx !== 0 && endIdx !== 0) temp = temp.slice(startIdx, endIdx);
-  else if (startIdx !== 0) temp = temp.slice(startIdx);
-  else if (endIdx !== 0) temp = temp.slice(0, endIdx);
+  if (startIdx < 0 && Math.abs(startIdx) < Math.abs(endIdx))
+    temp = temp.slice(endIdx, startIdx)
+  else if (startIdx !== 0 && endIdx > 0) temp = temp.slice(startIdx, endIdx)
+  else if (startIdx !== 0) temp = temp.slice(startIdx)
+  else if (endIdx !== 0) temp = temp.slice(0, endIdx)
 
   if (step > 1) {
-    temp = temp.filter((_, idx) => idx % step === 0);
+    temp = temp.filter((_, idx) => idx % step === 0)
   }
 
-  return temp;
+  return temp
 }
 
-Array.prototype.select = select;
-
-module.exports = select;
+module.exports = select
--- arrayselect.test.js
+++ arrayselect.test.js
@@ -1,9 +1,62 @@
-const select = require("./arrayselect.js");
+const select = require("./arrayselect.js")
+const lodash = require("lodash")
+
+Array.prototype.select = select
 
 describe("Testing prototype function", () => {
-  const mockedArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+  const mockedArr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-  it("shoult have a title", () => {
-    expect(mockedArr.select(0, 3)).toBe([0, 1, 2]);
-  });
-});
+  it("is example 0", () => {
+    const expectedValue = [0, 1, 2]
+    const output = mockedArr.select(0, 3)
+
+    const isEqualToExpectedValue = lodash.isEqual(expectedValue, output)
+
+    expect(isEqualToExpectedValue).toBeTruthy()
+  })
+
+  it("is example 1", () => {
+    const expectedValue = [1, 2, 3, 4]
+    const output = mockedArr.select(1, 5)
+
+    const isEqualToExpectedValue = lodash.isEqual(expectedValue, output)
+
+    expect(isEqualToExpectedValue).toBeTruthy()
+  })
+
+  it("is example 2", () => {
+    const expectedValue = [0, 2]
+    const output = mockedArr.select(0, 4, 2)
+
+    const isEqualToExpectedValue = lodash.isEqual(expectedValue, output)
+
+    expect(isEqualToExpectedValue).toBeTruthy()
+  })
+
+  it("is example 3", () => {
+    const expectedValue = [5, 6, 7, 8, 9]
+    const output = mockedArr.select(-5, 0, 0)
+
+    const isEqualToExpectedValue = lodash.isEqual(expectedValue, output)
+
+    expect(isEqualToExpectedValue).toBeTruthy()
+  })
+
+  it("is example 4", () => {
+    const expectedValue = [0, 1, 2, 3, 4]
+    const output = mockedArr.select(0, -5)
+
+    const isEqualToExpectedValue = lodash.isEqual(expectedValue, output)
+
+    expect(isEqualToExpectedValue).toBeTruthy()
+  })
+
+  it("is example 5", () => {
+    const expectedValue = [5, 6, 7]
+    const output = mockedArr.select(-2, -5)
+
+    const isEqualToExpectedValue = lodash.isEqual(expectedValue, output)
+
+    expect(isEqualToExpectedValue).toBeTruthy()
+  })
+})
